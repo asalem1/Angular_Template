660cd6f7b4b07c315a71229b24271142231b1dae
Ariel <Ariel.Salem1989@gmail.com>
2017-05-31 10:26:31 -0700

    initialized server

---
+++ Server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+// const db = require('../Database/list_model');
+const app = express();
+
+/* CONNECT TO THE SERVER */
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/Client'));
+app.use(bodyParser.json());
+
+/* CONFIGURE OUR ROUTES */
+require('./Routes/*')(app);
+
+/* START APP */
+app.listen(port, () => {
+  console.log('we have hacked into the mainframe: ', port);
+});
+
+module.exports = app;
+
