21499c99974f949aa623109ec3b208dd60ee2215
Ariel <Ariel.Salem1989@gmail.com>
2017-06-20 09:31:18 -0700

    removed unnecessary packages

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const mongojs = require('mongojs');
-const mLab = require('../mlab');
 const db = mongojs('tasks', ['tasks']);
 
 // Get All Tasks
